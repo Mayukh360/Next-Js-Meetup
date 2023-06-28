6bd6cc2dc78882e053511b680cfce83e6c706091
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-28 20:08:35 +0530

    getStaicProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,3 +13,42 @@ export default function MeetupDetail() {
     </>
   );
 }
+
+export async function getStaticPaths(){
+
+  return{
+    fallback:true,
+    paths:[
+      {
+        params:{
+          meetupId:'m1',
+        },
+      },
+      {
+        params:{
+          meetupId:'m2',
+        },
+      },
+    ]
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://images.unsplash.com/photo-1618823617088-b170e5d7501a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1183&q=80",
+        title: "First Meetup",
+        address: "205 London,Uk,",
+        description: "This is the First Meetup",
+      },
+    },
+  };
+}
