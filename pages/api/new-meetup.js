af9d387382cdc4fc918fbdb76b48fc99a7baa593
Mayukh <mayukhchatterjee2023@gmail.com>
2023-09-11 14:33:14 +0530

    Final

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   if (req.method == "POST") {
     const data = req.body;
-     // const {title,image,address,description}=data
+    // const {title,image,address,description}=data
     const client = await MongoClient.connect(
       "mongodb+srv://mkc360:m.c.605551@cluster0.mxwuzmm.mongodb.net/meetups?retryWrites=true&w=majority"
     );
@@ -15,10 +15,6 @@ async function handler(req, res) {
     client.close();
     res.status(201).json({ message: "Request successful" });
   }
-
-  
-
- 
 }
 
 export default handler;
